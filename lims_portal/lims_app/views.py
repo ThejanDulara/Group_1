d03e80bbe291414f5a6d6b36bd614152a4bc7c23
Janani Halwala <100335233+halwala@users.noreply.github.com>
2024-06-16 21:55:12 +0530

    commit_3

--- lims_portal/lims_app/views.py
+++ lims_portal/lims_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+from .models import *
+# from .forms import *
+from .forms import StudentProfileForm, UserRegistrationForm
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import HttpResponseForbidden
