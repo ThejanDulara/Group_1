acbd57e781ac3e07a4f14d4a36f116d322306f8e
Thejan Dulara <thejangammanpila@gmail.com>
2024-06-21 00:15:00 +0530

    Created Path

--- lims_portal/lims_app/urls.py
+++ lims_portal/lims_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_books/', views.add_books, name='add_books'),
+    path('Add Books/', views.add_books, name='add_books'),
+    path('Home/', views.landing_page, name='Home'),
+    path('Register/', views.Register, name='Register'),
+    path('Login/', views.Login, name='Login'),
 ]
 
 
--- lims_portal/lims_app/views.py
+++ lims_portal/lims_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import AddBookForm
 
+
 def add_books(request):
     if request.method == 'POST':
         form = AddBookForm(request.POST)
@@ -11,3 +12,37 @@ def add_books(request):
         form = AddBookForm()
 
     return render(request, 'lims_app/add_books.html', {'form': form})
+
+
+def landing_page(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/landing_page.html')
+
+def Register(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/Registration.html')
+
+def Login(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/Login.html')
