15e793b05c18d383f7fdb829a30b0e639a514b35
Thejan Dulara <thejangammanpila@gmail.com>
2024-06-20 23:27:34 +0530

    add_books request added

--- lims_portal/lims_app/views.py
+++ lims_portal/lims_app/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.decorators import login_required
+from .forms import AddBookForm
 
-def user_login(request):
+def add_books(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = AddBookForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index')
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
     else:
-        form = AuthenticationForm()
-    return render(request, 'library/login.html', {'form': form})
+        form = AddBookForm()
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'library/Registration.html', {'form': form})
+    return render(request, 'lims_app/add_books.html', {'form': form})
