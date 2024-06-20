50d4f96232418aca0d3b0aa595c8f85f9b0e54a0
Thejan Dulara <thejangammanpila@gmail.com>
2024-06-20 23:21:18 +0530

    AddBook class created

--- lims_portal/lims_app/models.py
+++ lims_portal/lims_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class AddBook(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    rack_number = models.CharField(max_length=50)
+    category = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    description = models.TextField()
+
