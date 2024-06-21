d399fdd76fd05496200d370d51e1333429d54e32
Janani Halwala <100335233+halwala@users.noreply.github.com>
2024-06-21 22:59:24 +0530

    views.py

--- lims_portal/lims_app/views.py
+++ lims_portal/lims_app/views.py
@@ -1,1 +1,59 @@
+from django.shortcuts import render, redirect
+from .forms import AddBookForm
 
+
+def add_books(request):
+    if request.method == 'POST':
+        form = AddBookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()
+
+    return render(request, 'lims_app/add_books.html', {'form': form})
+
+
+def Home(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/landing_page.html')
+
+def Register(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/Registration.html')
+
+def Login(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/Login.html')
+
+def Staff(request):
+    """if request.method == 'POST':
+        form = LanfingPageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_books')  # Redirect to the same page after saving
+    else:
+        form = AddBookForm()"""
+
+    return render(request, 'lims_app/staff_home.html')
